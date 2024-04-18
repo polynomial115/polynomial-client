237b62dcd7dd284d4bc2fd128a0cd8bfe5382c13
advaith <advaithj1@gmail.com>
2024-04-18 16:05:05 -0700

    support local dev without tunneling

--- src/discord.ts
+++ src/discord.ts
@@ -2,9 +2,11 @@ import { DiscordSDK, patchUrlMappings } from '@discord/embedded-app-sdk'
 
 export const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID)
 
-patchUrlMappings([
-	{
-		prefix: '/googleapis/{subdomain}',
-		target: '{subdomain}.googleapis.com'
-	}
-])
+if (import.meta.env.PROD) {
+	patchUrlMappings([
+		{
+			prefix: '/googleapis/{subdomain}',
+			target: '{subdomain}.googleapis.com'
+		}
+	])
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    supported: {
-      'top-level-await': true
-    },
+  plugins: [react(), mkcert()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1999',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
