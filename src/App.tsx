12fd19272d512d3edebc7a325a7d8d4f4349f9a6
advaith <advaithj1@gmail.com>
2024-04-29 11:36:05 -0700

    remove mui bar and update deps

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,6 @@ import withReactContent from 'sweetalert2-react-content'
 import { CreateProject } from './components/CreateProject.tsx'
 import { ProjectView } from './ProjectView'
 
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
-
 const swal = withReactContent(Swal)
 
 const sendCount = (count: number) => {
@@ -46,63 +43,46 @@ function App() {
 	const auth = useAuth()
 
 	return (
-		<>
-			<div className="AppBarCSS">
-				<AppBar position="static">
-					<Toolbar color="primary">
-						<IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-							<MenuIcon />
-						</IconButton>
-
-						<Typography variant="h6" color="inherit" component="div">
-							Polynomial Dashboard - &nbsp;
-						</Typography>
-						<h3>{channel}</h3>
-					</Toolbar>
-				</AppBar>
-			</div>
-
-			<div className="RootProject">
-				<h1>{channel}</h1>
-				<p>Projects: {projects.length}</p>
+		<div className="RootProject">
+			<h1>{channel}</h1>
+			<p>Projects: {projects.length}</p>
+			<button
+				onClick={() =>
+					swal.fire({
+						html: <CreateProject />,
+						background: '#202225',
+						color: 'white',
+						showConfirmButton: false
+					})
+				}
+			>
+				Create Project
+			</button>
+			<ProjectView></ProjectView>
+			<div className="card">
 				<button
-					onClick={() =>
-						swal.fire({
-							html: <CreateProject />,
-							background: '#202225',
-							color: 'white',
-							showConfirmButton: false
-						})
-					}
+					onClick={() => {
+						setCount(count => count + 1)
+						sendCount((count || 0) + 1)
+					}}
 				>
-					Create Project
+					Count is equal to the value of {count}
 				</button>
-				<ProjectView></ProjectView>
-				<div className="card">
-					<button
-						onClick={() => {
-							setCount(count => count + 1)
-							sendCount((count || 0) + 1)
-						}}
-					>
-						Count is equal to the value of {count}
-					</button>
-					<button
-						onClick={() => {
-							setCount(0)
-							sendCount(0)
-						}}
-					>
-						Reset Count
-					</button>
-					<p>Participants: {participants.map(p => p.username).join(', ')}</p>
-				</div>
-
-				<p className="read-the-docs">
-					Connected to Firebase as user {auth.claims.user_id as string} with roles {JSON.stringify(auth.claims.roles)}
-				</p>
+				<button
+					onClick={() => {
+						setCount(0)
+						sendCount(0)
+					}}
+				>
+					Reset Count
+				</button>
+				<p>Participants: {participants.map(p => p.username).join(', ')}</p>
 			</div>
-		</>
+
+			<p className="read-the-docs">
+				Connected to Firebase as user {auth.claims.user_id as string} with roles {JSON.stringify(auth.claims.roles)}
+			</p>
+		</div>
 	)
 }
 
