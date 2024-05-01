dea49e4da9333c871eef95d2a3d1e48c18b8074a
Cheuk Pui Lam <lcpmatthew@gmail.com>
2024-05-01 14:12:50 -0700

    fixed typing errors of any

--- src/components/CreateProject.tsx
+++ src/components/CreateProject.tsx
@@ -5,6 +5,7 @@ import Select from 'react-select'
 import { selectStyles } from '../styles/select-styles.ts'
 import { Timestamp, addDoc, collection } from 'firebase/firestore'
 import { db } from '../services/firebase.ts'
+import { SelectOptionType } from '../types.ts'
 
 const transformColor = (color: number) => (color ? '#' + color.toString(16).padStart(6, '0') : 'white')
 
@@ -29,7 +30,7 @@ export function CreateProject() {
 		<div style={{ height: '400px' }}>
 			<h2>Creating new project</h2>
 			<form
-				onSubmit={async e => {
+				onSubmit={async (e: { preventDefault: () => void }) => {
 					e.preventDefault()
 					await addDoc(collection(db, 'projects'), {
 						guildId: discordSdk.guildId,
@@ -50,13 +51,10 @@ export function CreateProject() {
 					isMulti
 					options={roles.map(r => ({ value: r.id, label: r.name, color: transformColor(r.color) }))}
 					styles={selectStyles}
-					onChange={selected => setSelectedRoles(selected.map(e => e.value as string))}
+					onChange={(selected: SelectOptionType[]) => setSelectedRoles(selected.map((e: { value: string }) => e.value))}
 					name="roles"
 				/>
 				<button type="submit">Submit</button>
-				{/* <Select 
-				isMulti
-				optinos = {users.map(u)}></Select> */}
 			</form>
 		</div>
 	)
