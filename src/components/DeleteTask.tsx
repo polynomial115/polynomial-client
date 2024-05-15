54a401a4f436f7dc941a39bc4fbbfc4d947ee07b
Cheuk Pui Lam <lcpmatthew@gmail.com>
2024-05-15 16:53:07 -0700

    chore: updated swal behavior after yes/ no

--- src/components/DeleteTask.tsx
+++ src/components/DeleteTask.tsx
@@ -1,7 +1,7 @@
 import { Task } from '../types'
 import { doc, updateDoc } from 'firebase/firestore'
 import { db } from '../services/firebase.ts'
-import { useState } from 'react'
+import Swal from 'sweetalert2'
 interface Props {
 	projectId: string
 	tasks: Task[]
@@ -10,28 +10,16 @@ interface Props {
 
 export function DeleteTask({ projectId, tasks, delTask }: Props) {
 	const projectDoc = doc(db, 'projects', projectId)
-	const [deleted, setDeleted] = useState(false)
-	const [ok, setok] = useState(false)
-	// const []
+
 	const handleYes = async () => {
 		await updateDoc(projectDoc, { tasks: tasks.filter(t => t.id != delTask.id) })
-		setDeleted(true)
-	}
-	// if deleted
-	const handleNo = () => {
-		setok(true)
-	}
-	if (deleted) {
-		return <div>Successful</div>
-	}
-	if (ok) {
-		return <div>ok</div>
+		Swal.close()
 	}
 	return (
 		<div>
 			<h2>Are you sure you want to delete {delTask.name}?</h2> <br />
 			<button onClick={() => handleYes()}> Yes </button>
-			<button onClick={() => handleNo()}> No </button>
+			<button onClick={() => Swal.close()}> No </button>
 		</div>
 	)
 }
