49351e5a6d05523a047d0eba83337489af34b149
hab927 <warmairballoon@gmail.com>
2024-05-21 23:17:12 -0700

    styling on the modal

--- src/components/TaskDetails.tsx
+++ src/components/TaskDetails.tsx
@@ -7,11 +7,42 @@ interface Props {
 export default function TaskDetails({ task }: Props) {
 	return (
 		<div style={{ padding: '20px' }}>
-			<h1>{task.name}</h1>
-			<h3>ID</h3>
-			<p>{task.id}</p>
-			<h3>Description</h3>
-			<p>{task.description}</p>
+			<h2
+				style={{
+					margin: '0px',
+					fontSize: '36px'
+				}}
+			>
+				{task.name}
+			</h2>
+			<h3
+				style={{
+					marginBottom: '4px'
+				}}
+			>
+				ID
+			</h3>
+			<p
+				style={{
+					marginTop: '0px'
+				}}
+			>
+				{task.id}
+			</p>
+			<h3
+				style={{
+					marginBottom: '4px'
+				}}
+			>
+				Description
+			</h3>
+			<p
+				style={{
+					marginTop: '0px'
+				}}
+			>
+				{task.description}
+			</p>
 		</div>
 	)
 }
